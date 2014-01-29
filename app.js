28e8c6ecec962f90f0ef417e5761aca269bee417
Kent Andersen <kent.andersen1@sparebank1.no>
2014-01-29 09:27:30 +0100

    added webserver and startup rutine

---
+++ app.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+
+var port = Number(process.env.PORT || 5000);
+
+var static = require('node-static');
+
+var fileServer = new static.Server('./build');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}).listen(port);
+
+console.log("Server started on port " + port);
